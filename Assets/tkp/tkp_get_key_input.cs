c35f896a2b3a3df60f1c54954ba2b23b793f6102
tkp239 <taka11.pon11@gmail.com>
2023-01-05 20:05:48 +0900

    rotate

--- Assets/tkp/tkp_get_key_input.cs
+++ Assets/tkp/tkp_get_key_input.cs
@@ -7,13 +7,19 @@ public class tkp_get_key_input : MonoBehaviour
 {
     private GameObject cube_game_object;
     private GameObject ground_object;
+    private Rigidbody rb;
     private string input_key;
     private Vector3 cube_position;
+    private Vector3 cube_rotation;
     private float velocity = 0.01f;
+    private float obj_rotation = 0.1f;
+    private bool isground = false;
     // Start is called before the first frame update
     void Start()
     {
         cube_game_object = this.gameObject;
+        rb = GetComponent<Rigidbody>();
+        cube_rotation = new Vector3(0, obj_rotation, 0);
         ground_object = GameObject.Find("ground");
         Debug.Log(cube_game_object.name);
     }
@@ -22,6 +28,7 @@ public class tkp_get_key_input : MonoBehaviour
     void Update()
     {
         cube_position = cube_game_object.transform.position;
+        
         /*cube_position.x += velocity;
         cube_game_object.transform.position = cube_position;
         Debug.Log(cube_position);*/
@@ -57,12 +64,22 @@ public class tkp_get_key_input : MonoBehaviour
                     break;
 
                 case "Space":
-                    
-                    cube_position.y += velocity;
+                   if (isground)
+                   {
+                        rb.AddForce(new Vector3(0,20,0));
+                   }                    
                     break;
 
                 case "LeftControl":
                     cube_position.y -= velocity;
+                    break;
+
+                case "Q":
+                    cube_game_object.transform.Rotate(new Vector3(0, obj_rotation, 0));
+                    break;
+
+                case "E":
+                    cube_game_object.transform.Rotate(new Vector3(0, -obj_rotation, 0));
                     break;
 
                 default:
@@ -73,4 +90,25 @@ public class tkp_get_key_input : MonoBehaviour
         cube_game_object.transform.position = cube_position;
 
     }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.name == "ground")
+        {
+            isground = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "ground")
+        {
+            isground = false;
+        }
+    }
 }
+
+
+
+
+
