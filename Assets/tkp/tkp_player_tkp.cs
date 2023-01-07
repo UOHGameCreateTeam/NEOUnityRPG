75983b5c1997c99f05597f38f14de5f41d9ca0e4
tkp239 <taka11.pon11@gmail.com>
2023-01-07 16:13:19 +0900

    player

---
+++ Assets/tkp/tkp_player_tkp.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tkp_player_tkp : MonoBehaviour
+{
+    [SerializeField] private int hp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hp = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public int get_hp()
+    {
+        return hp;
+    }
+    public void set_hp(int set_values)
+    {
+        hp = set_values;
+    }
+}
