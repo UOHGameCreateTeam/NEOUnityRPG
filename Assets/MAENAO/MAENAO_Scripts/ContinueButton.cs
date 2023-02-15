30cd154180c6f16269e3a4878e41e3ba7c83f1ed
tb18knm <nao11-nkdmf346@outlook.jp>
2023-02-15 20:59:07 +0900

    update

--- Assets/MAENAO/MAENAO_Scripts/ContinueButton.cs
+++ Assets/MAENAO/MAENAO_Scripts/ContinueButton.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ContinueButton : MonoBehaviour
 {
+    [SerializeField] GameObject textObject;
+    private Text ContinueText;
+    private Color currentContinueColor;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ContinueText = textObject.GetComponent<Text>();
+        currentContinueColor = ContinueText.color;
     }
 
     // Update is called once per frame
@@ -20,6 +25,15 @@ public class ContinueButton : MonoBehaviour
     {
         SceneManager.LoadScene("hugehugeScene"); // 遷移したいScene名を入れる
         Debug.Log("ボタン押した");
+    }
+
+    public void ChangeContinueColor() // カーソルが乗ったら色変わる
+    {
+        ContinueText.color = new Color(255f, 255f, 255f, 255f);
+    }
+    public void ResetContinueCloror() // カーソルが離れたら元の色に戻る
+    {
+        ContinueText.color = currentContinueColor;
     }
 
 }
--- Assets/MAENAO/MAENAO_Scripts/PlayerHpSlider.cs
+++ Assets/MAENAO/MAENAO_Scripts/PlayerHpSlider.cs
@@ -9,10 +9,14 @@ public class PlayerHpSlider : MonoBehaviour
     [SerializeField] private GameObject playerObj;
     [SerializeField] public Slider redSlider;
     [SerializeField] public Slider greenSlider;
+    [SerializeField] public Slider warningSlider;
+    [SerializeField] public Animator anim;
 
     public bool gameOver = false;
     private int maxPlayerHp;
     private int currentPlayerHp;
+    private int getPlayerHp = 100;
+    private int damage;
     private tkp_player_tkp playerClass;
 
 
@@ -21,9 +25,14 @@ public class PlayerHpSlider : MonoBehaviour
     void Start()
     {
         playerClass = playerObj.GetComponent<tkp_player_tkp>();
-        maxPlayerHp = playerClass.get_hp();
+        anim = gameObject.GetComponent<Animator>();
+        //maxPlayerHp = playerClass.get_hp();
+        maxPlayerHp = 100;
+        currentPlayerHp = 100;
+        damage = 0;
         greenSlider.value = 1;
         redSlider.value = 1;
+        warningSlider.value = 0;
         //Debug.Log("Start current HP" + maxPlayerHp);
     }
 
@@ -32,10 +41,33 @@ public class PlayerHpSlider : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        currentPlayerHp = playerClass.get_hp();
+        //currentPlayerHp = playerClass.get_hp();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            getPlayerHp = currentPlayerHp - 2;
+        }
 
-        greenSlider.value = currentPlayerHp / maxPlayerHp;
-        StartCoroutine(ResetRedSlider());
+        //getPlayerHp = playerClass.get_hp(); // ダメージ受けてHP減ったときの残りHP
+        if (currentPlayerHp - getPlayerHp > 0)
+        {
+            damage = currentPlayerHp - getPlayerHp;
+            currentPlayerHp = getPlayerHp;
+        }
+
+        greenSlider.value = (float)currentPlayerHp / (float)maxPlayerHp;
+        if (damage > 0)
+        {
+            StartCoroutine(ResetRedSlider());
+            damage = 0;
+        }
+        
+        
+
+        if (currentPlayerHp <= maxPlayerHp * 0.05f)
+        {
+            warningSlider.value = 1;
+            anim.SetBool("boolwarning", true);
+        }
 
         if (currentPlayerHp <= 0)
         {
@@ -45,11 +77,11 @@ public class PlayerHpSlider : MonoBehaviour
 
 
 
-
-
     private IEnumerator ResetRedSlider()
     {
-        yield return new WaitForSeconds(1);
-        redSlider.value = currentPlayerHp / maxPlayerHp;
+        yield return new WaitForSeconds(1.0f);
+        redSlider.value = (float)currentPlayerHp / (float)maxPlayerHp;
+        yield break;
+
     }
 }
