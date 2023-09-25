6bd57ce55b9129c82439e36311fc96e53174e276
ATtami YYNa <tami19996@gmail.com>
2023-09-25 21:53:11 +0900

    update

---
+++ Assets/TAMIYANOMAR/Script/TAMIYANOMAR_enemy2_move.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TAMIYANOMAR_enemy2_move : MonoBehaviour
+{
+    [SerializeField] GameObject attack_cube;
+    [SerializeField] GameObject rush_cube;
+    [SerializeField] GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
